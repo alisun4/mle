908abf48da495e3242fc21fc5cb2fcc2a4fe0791
alisun4 <166564068+alisun4@users.noreply.github.com>
2024-05-14 18:40:36 -0700

    initial 2

---
+++ A2-Data/A2-Data/main.py
@@ -0,0 +1,64 @@
+from utils import *
+import numpy as np
+import argparse
+
+def get_features(filename, feat_extractor):
+    f = open(filename)
+    text_set = f.readlines()
+    f.close()
+    
+    features = []
+    i = 0
+    for text in text_set:
+        feature_vect = feat_extractor.transform(tokenize(text))
+        if len(feature_vect) > 23729:
+            print(f"New words added on example {i}.")
+            print(len(feature_vect))
+            return np.array(features)
+        features.append(feature_vect)
+        i += 1
+    
+    # print(features[:5])
+    
+    return np.array(features)
+
+def perplexity(features, probs):
+    return np.exp(np.dot(features, np.transpose(probs)))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--feature', '-f', type=str, default='unigram',
+                        choices=['unigram', 'bigram', 'customized'])
+    args = parser.parse_args()
+    if args.feature == "unigram":
+        feat_extractor = UnigramFeature()
+    elif args.feature == "bigram":
+        feat_extractor = BigramFeature()
+        
+    f = open("1b_benchmark.train.tokens")
+    train_set = f.readlines()
+    f.close()
+    
+    training_data = []
+    
+    for text in train_set:
+        training_data.append(tokenize(text))
+    
+    feat_extractor.fit(training_data)
+    
+    # print(len(feat_extractor.unigram))
+
+    train_features = get_features("1b_benchmark.train.tokens", feat_extractor)
+    
+    # print(train_features.shape)
+    
+    train_log_probs = feat_extractor.token_log_probs(train_features)
+    
+    perp = perplexity(train_features, train_log_probs)
+    
+    print(perp)
+    
+    
+
+if __name__ == "__main__":
+    main()
---
+++ A2-Data/A2-Data/utils.py
@@ -0,0 +1,164 @@
+from nltk.tokenize import regexp_tokenize
+import numpy as np
+from collections import defaultdict
+
+default_pattern =  r"""(?x)                  
+                        (?:[A-Z]\.)+          
+                        |\$?\d+(?:\.\d+)?%?    
+                        |\w+(?:[-']\w+)*      
+                        |\.\.\.               
+                        |(?:[.,;"'?():-_`])    
+                    """
+
+def tokenize(text, pattern = default_pattern):
+    """Tokenize senten with specific pattern
+    
+    Arguments:
+        text {str} -- sentence to be tokenized, such as "I love NLP"
+    
+    Keyword Arguments:
+        pattern {str} -- reg-expression pattern for tokenizer (default: {default_pattern})
+    
+    Returns:
+        list -- list of tokenized words, such as ['I', 'love', 'nlp']
+    """
+    text = text.lower()
+    return regexp_tokenize(text, pattern)
+
+class FeatureExtractor(object):
+    """Base class for feature extraction.
+    """
+    def __init__(self):
+        pass
+    def fit(self, text_set):
+        pass
+    def transform(self, text):
+        pass  
+    def transform_list(self, text_set):
+        pass
+
+
+
+class UnigramFeature(FeatureExtractor):
+    """Example code for unigram feature extraction
+    """
+    def __init__(self):
+        self.unigram = {}
+        
+    def zero(self):
+        return 0
+    
+    def two(self):
+        return 2
+        
+    def fit(self, text_set: list):
+        """Fit a feature extractor based on given data 
+        
+        Arguments:
+            text_set {list} -- list of tokenized sentences and words are lowercased, such as [["I", "love", "nlp"], ["I", "like", "python"]]
+        """
+        self.unigram["<START>"] = 0
+        self.unigram["<STOP>"] = 1
+        self.unigram["<UNC>"] = 2
+        index = 3
+        count = defaultdict(self.zero)
+        for i in range(0, len(text_set)):
+            for j in range(0, len(text_set[i])):
+                count[text_set[i][j]] += 1
+                if count[text_set[i][j]] == 3:
+                    self.unigram[text_set[i][j].lower()] = index
+                    index += 1
+                else:
+                    continue
+                    
+    def transform(self, text: list):
+        """Transform a given sentence into vectors based on the extractor you got from self.fit()
+        
+        Arguments:
+            text {list} -- a tokenized sentence (list of words), such as ["I", "love", "nlp"]
+        
+        Returns:
+            array -- an unigram feature array, such as array([1,1,1,0,0,0])
+        """
+        feature = np.zeros(len(self.unigram))
+        feature[0] += 1
+        feature[1] += 1
+        for i in range(0, len(text)):
+            if text[i].lower() in self.unigram:
+                feature[self.unigram[text[i].lower()]] += 1
+            else:
+                feature[2] += 1
+        
+        return feature
+    
+    def transform_list(self, text_set):
+        # Add your code here!
+        features = []
+        for i in range(0, len(text_set)):
+            features.append(self.transform(text_set[i]))
+        
+        return np.array(features)
+
+    def token_log_probs(self, features):
+        return np.log(np.sum(features, axis = 0)) - np.log(np.sum(features))
+
+class BigramFeature(FeatureExtractor):
+    """Example code for unigram feature extraction
+    """
+    def __init__(self):
+        self.unigram = defaultdict(self.two)
+        
+    def zero(self):
+        return 0
+    
+    def two(self):
+        return 2
+        
+    def fit(self, text_set: list):
+        """Fit a feature extractor based on given data 
+        
+        Arguments:
+            text_set {list} -- list of tokenized sentences and words are lowercased, such as [["I", "love", "nlp"], ["I", "like", "python"]]
+        """
+        self.unigram["<START>"] = 0
+        self.unigram["<STOP>"] = 1
+        self.unigram["<UNC>"] = 2
+        index = 3
+        count = defaultdict(self.zero)
+        for i in range(0, len(text_set)):
+            for j in range(0, len(text_set[i])):
+                count[text_set[i][j]] += 1
+                if count[text_set[i][j]] == 3:
+                    self.unigram[text_set[i][j].lower()] = index
+                    index += 1
+                else:
+                    continue
+                    
+    def transform(self, text: list):
+        """Transform a given sentence into vectors based on the extractor you got from self.fit()
+        
+        Arguments:
+            text {list} -- a tokenized sentence (list of words), such as ["I", "love", "nlp"]
+        
+        Returns:
+            array -- an unigram feature array, such as array([1,1,1,0,0,0])
+        """
+        feature = np.zeros((len(self.unigram), len(self.unigram)))
+        feature[self.unigram[0]][self.unigram[text[0]]] += 1
+        for i in range(0, len(text)-1):
+            feature[self.unigram[text[i].lower()]][text[i+1].lower()] += 1
+        feature[self.unigram[text[-1].lower()]][self.unigram[1]] += 1
+        
+        return feature
+    
+    def transform_list(self, text_set):
+        # Add your code here!
+        features = []
+        for i in range(0, len(text_set)):
+            features.append(self.transform(text_set[i]))
+        
+        return np.array(features)
+    
+    def token_log_probs(self, features):
+        return np.log(np.sum(features, axis=0)) - np.log(np.sum(features, axis=(0,1)))
+    
